add0b110a671c4c1e67850e3348a3c72d76ad00f
Leandro Adrian da Silva <103830032+lezzin@users.noreply.github.com>
2024-03-11 00:09:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const routes = [
-    { path: '/', component: { template: '#s_inicio' } },
-    { path: '/materiais', component: { template: '#s_materiais' } },
-    { path: '/aprender', component: { template: '#s_aprender' } },
-    { path: '/ferramentas', component: { template: '#s_ferramentas' } },
-    { path: '/design', component: { template: '#s_design' } },
-    { path: '/plataformas', component: { template: '#s_plataformas' } }
+    {
+        path: '/',
+        component: {
+            template: '#s_inicio'
+        }
+    },
+    {
+        path: '/materiais',
+        component: {
+            template: '#s_materiais'
+        }
+    },
+    {
+        path: '/aprender',
+        component: {
+            template: '#s_aprender'
+        }
+    },
+    {
+        path: '/ferramentas',
+        component: {
+            template: '#s_ferramentas'
+        }
+    },
+    {
+        path: '/plataformas',
+        component: {
+            template: '#s_plataformas'
+        }
+    }
 ];
 
-const router = new VueRouter({
-    routes
-});
-
-const app = new Vue({
-    router,
-    beforeCreate() {
-        this.$router.push('/');
-    }
-}).$mount('#app');
+const router = new VueRouter({ routes });
+const app = new Vue({ router, }).$mount('#app');
