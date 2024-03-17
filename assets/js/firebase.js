fdf22951c605fbf99b0f6f87c7e063b0fada2404
Leandro Adrian da Silva <103830032+lezzin@users.noreply.github.com>
2024-03-17 11:37:18 -0300

    Add files via upload

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCAVN9xNa-bBNqxay8F5Hnbp4MIbk6eI7I",
-    authDomain: "ferramentas-ve.firebaseapp.com",
-    projectId: "ferramentas-ve",
-    storageBucket: "ferramentas-ve.appspot.com",
-    messagingSenderId: "180825406497",
-    appId: "1:180825406497:web:df580c19a3b2262324b760",
-    measurementId: "G-D0485WHLXT"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
