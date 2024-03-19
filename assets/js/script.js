706b7e732accdbb4fd1efc1372829b70843fb215
Leandro Adrian da Silva <103830032+lezzin@users.noreply.github.com>
2024-03-18 22:07:09 -0300

    Add articles to homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -379,6 +379,34 @@ const Login = {
 // Vue component for the home page
 const Home = {
     template: '#s_inicio',
+    data() {
+        return {
+            article_tips: [],
+            article_tutorials: []
+        };
+    },
+    methods: {
+        async fetchArticles(type) {
+            try {
+                const response = await fetch(`https://dev.to/api/articles?tag=${type}&language=pt&per_page=10`);
+                const data = await response.json();
+                return data;
+            } catch (error) {
+                this.$root.toast = {
+                    type: "error",
+                    text: "Erro na solicitação de artigos: " + error
+                };
+                return null;
+            }
+        },
+        async loadArticles() {
+            this.article_tips = await this.fetchArticles("tip");
+            this.article_tutorials = await this.fetchArticles("tutorial");
+        }
+    },
+    created() {
+        this.loadArticles();
+    }
 };
 
 // Vue component for displaying a topic
@@ -518,7 +546,7 @@ const app = new Vue({
             try {
                 await auth.signOut();
                 this.user = null;
-                if(this.$router.history.current.fullPath != '/') {
+                if (this.$router.history.current.fullPath != '/') {
                     this.$router.push('/');
                 }
             } catch (error) {
