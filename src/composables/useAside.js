90f1a5d90bd4d1a5d6dfab397e0f4733629d9c79
Leandro Adrian da Silva <leandrinsilva22@gmail.com>
2024-11-13 10:17:03 -0300

    remove aside functions from useAside

--- src/composables/useAside.js
+++ src/composables/useAside.js
@@ -2,11 +2,11 @@ import { ref } from "vue";
 
 const isActive = ref(false);
 
-export const useAside = () => {
-    const toggleMenu = () => (isActive.value = !isActive.value);
-    const closeMenu = () => (isActive.value = false);
-    const showMenu = () => (isActive.value = true);
+const toggleMenu = () => (isActive.value = !isActive.value);
+const closeMenu = () => (isActive.value = false);
+const showMenu = () => (isActive.value = true);
 
+export const useAside = () => {
     return {
         isActive,
         toggleMenu,
