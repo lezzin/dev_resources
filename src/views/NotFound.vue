46ec1f5fe53d516ba8f9adb7837e489672465c81
Leandro Adrian da Silva <leandrinsilva22@gmail.com>
2024-11-12 21:46:32 -0300

    remove images

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,31 +1,14 @@
+<script setup lang="ts">
+import { QBtn, QIcon } from 'quasar';
+</script>
+
 <template>
-    <section>
-        <div class="column justify-center items-center">
-            <img src="../assets/img/not_found_lg.gif" alt="Imagem de um robô e um computador com o número 404 na tela"
-                width="900" height="454" class="lg">
-            <img src="../assets/img/not_found_sm.gif" alt="Imagem de um robô e um computador com o número 404 na tela"
-                width="568" height="454" class="sm">
-            <RouterLink class="link" to="/">Voltar para o início</RouterLink>
+    <section class="q-pa-md flex flex-center">
+        <div class="text-center">
+            <h1 class="text-h1 q-my-md">404</h1>
+            <h2 class="text-h4 q-mt-md">Oops! Página não encontrada</h2>
+            <p>A página que você está procurando não existe ou foi movida.</p>
+            <QBtn label="Ir para o início" color="primary" @click="$router.push('/')" class="q-mt-lg" />
         </div>
     </section>
 </template>
-
-<style scoped>
-.lg {
-    display: initial;
-}
-
-.sm {
-    display: none;
-}
-
-@media(max-width: 768px) {
-    .lg {
-        display: none;
-    }
-
-    .sm {
-        display: initial;
-    }
-}
-</style>
